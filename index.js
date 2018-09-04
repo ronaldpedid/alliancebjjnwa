4fd1352b78a09202beeace40a9cb15bd4b41fb4d
Ronald Pedid <ronaldpedid@live.com>
2018-09-04 12:37:06 -0500

    'attempt2'

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ function initializeApp() {
   let PORT = 8000;
   let PORT_ENV = process.env.PORT;
 
-  app.use('/dist', express.static(path.join(__dirname, 'public')));
+
 
   /* production only */
   if (process.env.NODE_ENV === 'production') {
     // Express will serve up production assets ie: main.css/main.js
     app.use(express.static('client/dist'));
+    app.use('/dist', express.static(path.join(__dirname, 'public')));
     //express will serve up the index.html file if it doesn't recognize the route.
     app.get('/*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
