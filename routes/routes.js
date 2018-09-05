f5dff284555c5a66851f44ec1b546783a39afd4c
Ronald Pedid <ronaldpedid@live.com>
2018-09-05 07:09:57 -0500

    all updates made yesterday

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,45 @@
 module.exports = (app) => {
-  app.get('/', function (req, res) {
-    res.sendFile("src/index.html");
+  app.post('/api/form', (req, res) => {
+    nodemailer.createTestAccount((err, account) => {
+      const htmlEmail = `
+        <h3>Contact Details</h3>
+        <ul>
+          <li>${req.body.name}</li>
+          <li>${req.body.email}</li>
+          <li>${req.body.phone}</li>
+          
+        </ul>
+        <h3>Message</h3>
+        <p>${req.body.message}</p>
+        `;
+
+      let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 465,
+        auth: {
+          user: keys.googleUserName,
+          pass: keys.googleEmailPassword
+        }
+      })
+
+      let mailOptions = {
+        from: 'noreply@alliancebjjofnwa.com',
+        to: 'elementfayt@gmail.com',
+        replyTo: 'noreply@alliancebjjofnwa.com',
+        subject: 'BJJ Website Reply',
+        text: req.body.message,
+        html: htmlEmail
+      }
+
+      transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log('Message Sent');
+      })
+
+    })
   });
 
 }
