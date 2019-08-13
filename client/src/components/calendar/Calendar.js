f11f688437e323e341cd73cb6091a480b26cba37
Ronald Pedid <ronaldpedid@live.com>
2019-08-13 17:39:02 -0500

    new build

--- client/src/components/calendar/Calendar.js
+++ client/src/components/calendar/Calendar.js
@@ -4,7 +4,16 @@ import Media from 'react-media';
 export class Calendar extends Component {
   makeKey(index) {
     let random1 = ['1', 'b', 'q', 's', '8', '90', 'qw'];
-    let roll = Math.floor(Math.random() * 7000 / index);
+    let roll = Math.floor(Math.random() * 700000 / index);
+    roll.toString();
+    console.log(roll.length);
+    if (roll.length > 10) {
+      roll = null;
+      this.makeKey(index);
+    } else {
+      let numRoll = parseInt(roll);
+      return random1[numRoll];
+    }
     return random1[roll];
   }
   render() {
