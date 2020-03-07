59776c52869048c5de5f5cd3cbbdcccfb67b3199
Ronald Pedid <ronaldpedid@live.com>
2020-03-06 20:25:03 -0600

    emergency launch

--- client/src/components/landing/Landing.js
+++ client/src/components/landing/Landing.js
@@ -4,10 +4,9 @@ import style from './landing.scss';
 import { Header } from '../header/Header';
 import { Hero } from '../hero/Hero';
 import { Footer } from '../footer/Footer';
-import { MissionStatement } from '../mission/Mission';
 import Calendar from '../calendar/NewCalendar';
 import AllianceCarousel from '../carousel/Carousel';
- 
+
 
 export class Landing extends Component {
   render() {
@@ -15,8 +14,7 @@ export class Landing extends Component {
       <div className={style.backgroundLanding}>
         <Header />
         <Hero />
-        <MissionStatement />
-        <AllianceCarousel showArrows/>
+        <AllianceCarousel showArrows />
         <Calendar />
         <Footer />
       </div>
--- client/src/components/mission/Mission.js
+++ client/src/components/mission/Mission.js
@@ -8,10 +8,9 @@ export class MissionStatement extends Component {
         <div className={style.missionStatement}>
           <h1 className={style.missionHeader}>WE ARE ALLIANCE</h1>
           <p><em>
-              We at Alliance Jiu-Jitsu of Northwest Arkansas are excited to bring the
-               benefits of Brazilian Jiu-Jitsu to NWA. Whether your goal is weight loss, 
-               self defense, or high level competition. We are the team for you. Find out more about our 
-               programs below.  
+            We at Alliance Jiu-Jitsu of Northwest Arkansas are excited to bring the
+             benefits of Brazilian Jiu-Jitsu to NWA. Whether your goal is weight loss,
+             self defense, or high level competition. We are the team for you.
           </em></p>
         </div>
       </div>)
